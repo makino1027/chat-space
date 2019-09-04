d3acdb554ea420af5e3b86bd548a8f629d75a2cc
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-09-05 01:52:14 +0900

    コメントアウト削除

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,33 +1,5 @@
 $(function() {
-
-
-    
-  // var reloadMessages = function() {
-  //   if (window.location.href.match(/\/groups\/\d+\/messages/)){
-  //     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  //     last_message_id = $('.message:last').data('message-id');
-  //     $.ajax({
-  //       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-  //       url: 'api/messages#index {:format=>"json"}',
-  //       //ルーティングで設定した通りhttpメソッドをgetに指定
-  //       type: 'get',
-  //       dataType: 'json',
-  //       //dataオプションでリクエストに値を含める
-  //       data: {id: last_message_id}
-  //     })
-  //     .done(function(messages) {
-  //       console.log('success');
-  //     })
-  //     .fail(function() {
-  //       console.log('error');
-  //     });
-  //   };
-  // }  
-  // setInterval(reloadMessages, 5000);
-
-
    function buildHTML(message) {
-
     var content = message.content ? `<p class="lower-message__content">${message.content}</p>` : "";
     var img = message.image.url ? `<img class="lower-message__image" src= ${message.image.url}>` : "";
     var html = `<div class="message" data-message-id="${message.id}">
@@ -46,8 +18,6 @@ $(function() {
                 </div>`
       return html;
   }
-
-  
 
   function scrollBottom(){
     var target = $('.message').last();
