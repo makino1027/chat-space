3f4f9fce47d477968e832f7f52e937a83ce9d3ef
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-09-14 00:37:03 +0900

    修正１

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -33,10 +33,10 @@ $(document).on("turbolinks:load", function () {
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      $('.messages').append(html);
+    .done(function(data){   
       $('.form__submit').removeAttr("disabled");
       var html = buildHTML(data);
+      $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
       $('#new_message')[0].reset();
     })
