a76a1941ba5b9bff242408af9db0b4534fcf0bd5
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-09-14 12:43:33 +0900

    修正３

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -24,7 +24,7 @@ $(document).on("turbolinks:load", function () {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action'); 
-    $('#new_message').addClass('completed');
+    $('#new_message')[0].reset(); 
     $.ajax({
       url: url,
       type: 'POST',
@@ -36,7 +36,6 @@ $(document).on("turbolinks:load", function () {
     .done(function(data){   
       var html = buildHTML(data);
       $('.messages').append(html);   
-      $('#new_message')[0].reset(); 
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
     })
       .fail(function (data) {
@@ -66,8 +65,7 @@ $(document).on("turbolinks:load", function () {
           insertHTML = buildHTML(message);
           $('.messages').append(insertHTML);
           $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
-        })
-        
+        })        
       })      
         .fail(function () {
           alert('自動更新に失敗しました');
