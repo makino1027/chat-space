fb56f10da8fb89023d5f89ffa7f8ba76eb2d2d81
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-09-14 17:56:32 +0900

    修正12

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -53,30 +53,29 @@ $(document).on("turbolinks:load", function () {
  
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      var last_message_id = $('.message:last').data('message-id');
+      var last_message_id = $('.message:last').data('message-id') || 0;
       
       $.ajax({
-        url: 'api/messages#index',
+        url: 'api/messages',
         type: 'get',
         dataType: 'json',
         data: {message_id: last_message_id}
       })
-      .done(function (messages) {          
+      .done(function (messages) {
         var insertHTML = '';
-        messages.forEach(function (message) {
-          insertHTML = buildHTML(message);
-          $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
-        })        
+        if (messages.length > 0){
+          messages.forEach(function (message) {
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+          })       
+        } 
       })      
         .fail(function () {
           alert('自動更新に失敗しました');
         });
            
     };
-  }  
+  }
   setInterval(reloadMessages, 5000);
-
-
-
 });
